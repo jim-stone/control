5520aeb2a3d60247d2dd0168f20277f8797b1c8e
jim-stone <jakub.gustaw.kaminski@gmail.com>
2020-02-02 22:16:13 +0100

    Uncommented for heroku

--- kontrole/settings.py
+++ kontrole/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c)_uld=$5y_x1shmgt3%t(ol1ie(rk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['master-controller.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['master-controller.herokuapp.com', 
+    'damp-bastion-23462.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
